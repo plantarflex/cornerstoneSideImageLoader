c06b917756bc9d3794d1820d4bafcc714facd62d
plantarflex <plantarflex@naver.com>
2021-04-19 04:10:23 +0900

    add logic for umd pattern

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,21 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
-  mode: 'production',
-  output: {
+  mode: 'development',
+	output: 
+  {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
+    library: {
+      name: 'cornerstoneSideImageLoader',
+      type: 'umd',
+      umdNamedDefine: true,
+    },
+    clean: true,
   },
   optimization: {
     minimizer: [new TerserPlugin({
       extractComments: false,
     })],
-  },  
+  },
 };
