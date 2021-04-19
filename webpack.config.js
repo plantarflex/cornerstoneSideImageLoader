2339110c8e6b469cd98a25374187328fa78e136b
plantarflex <plantarflex@naver.com>
2021-04-20 03:52:57 +0900

    release production build

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
-  mode: 'development',
+  mode: 'production',
 	output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
