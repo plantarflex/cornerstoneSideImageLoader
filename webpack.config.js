5d8cdb0127178ddf6e67fea610474ac4cc033c48
plantarflex <plantarflex@naver.com>
2021-04-21 04:54:40 +0900

    fix external cornerstone issue

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
-  mode: 'production',
+  mode: 'development',
 	output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
